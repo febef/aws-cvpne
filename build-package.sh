c16ce543c9651dd94d5ec46320d792702fccfc5e
febef <febef.dev@gmail.com>
2023-09-29 18:46:04 +0000

    feat! (build): actualizar versión de OpenVPN
    - Actualizar el archivo openvpn-v2.5.2-aws.patch a openvpn-v2.6.6-aws.patch
    - Eliminar el archivo server-cvpn-sso
    - Compilar la imagen de OpenVPN con la versión 2.6.6
    - Actualizar el archivo openvpc-v2.6.6-aws.patch
    - Aumentar el tamaño máximo del buffer en buffer.h a 2^21
    - Aumentar el tamaño del buffer TLS_CHANNEL_BUF_SIZE a 2^18
    - Aumentar el tamaño del buffer ERR_BUF_SIZE a 2^18
    - Aumentar el tamaño del buffer de lectura del socket de gestión a 2^17
    - Aumentar el tamaño del buffer de nombre de usuario y contraseña a 2^17
    
    BREAKING CHANGE: Esta actualización cambia la versión de OpenVPN y modifica los tamaños de los buffers para mejorar el rendimiento y la capacidad de manejo de errores.

--- build-package.sh
+++ build-package.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 podman rmi openvpn-compiler
-rm ./aws-cvpne/usr/lib/aws-cvpne/openvpn-v2.5.2-aws.patch || true
+rm ./aws-cvpne/usr/lib/aws-cvpne/openvpn-v2.6.6-aws.patch || true
 rm ./aws-cvpne/usr/lib/aws-cvpne/server-cvpn-sso || true
 
 podman build . -f Dockerfile -t openvpn-compiler
 podman run -v ./:/pkg openvpn-compiler bash -c 'cd /pkg/source && ./build-openvpn.sh'
 
-cp -f ./source/openvpn/openvpn-2.5.2/src/openvpn/openvpn ./aws-cvpne/usr/lib/aws-cvpne/openvpn-v2.5.2-aws.patch
+cp -f ./source/openvpn/openvpn-2.6.6/src/openvpn/openvpn ./aws-cvpne/usr/lib/aws-cvpne/openvpn-v2.6.6-aws.patch
 cp -f ./source/server-cvpn-sso ./aws-cvpne/usr/lib/aws-cvpne/server-cvpn-sso
 
 dpkg-deb --build aws-cvpne
